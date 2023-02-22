ede7b8d3e8184dfbe5aaf02a5e4d13e91d54b7cf
endarmuh <2141720160@student.polinema.ac.id>
2023-02-22 09:59:19 +0700

    Perubahan 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    public function about() {
+        echo "Muhammad Endar Darmawan | 2141720160";
+    }
+}
