347dbb14ef5b6a5b18b1c76a6ce58db6cfbe10d7
endarmuh <2141720160@student.polinema.ac.id>
2023-02-22 09:50:25 +0700

    Perubahan 1

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,4 +7,15 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     //
+    public function index() {
+        echo "Selamat Datang";
+    }
+
+    public function about() {
+        echo "Muhammad Endar Darmawan | 2141720160";
+    }
+
+    public function articles($id) {
+        echo "Halaman Artikel dengan ID ".$id;
+    }
 }
